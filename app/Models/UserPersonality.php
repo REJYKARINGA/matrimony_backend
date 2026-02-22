4019abd13ef4d8f75c18821d864e91b9093fee2c
Rejy Karinga <rejykaringa2000@gmail.com>
2026-02-22 20:26:14 +0530

    user personality created

---
+++ app/Models/UserPersonality.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserPersonality extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_personality';
+
+    protected $fillable = [
+        'user_id',
+        'personality_id',
+    ];
+
+    /**
+     * Get the user
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
